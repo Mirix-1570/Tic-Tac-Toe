2e036f55151873128ff04537f61a50950401accd
Mirix-1570 <miricruz1570@gmail.com>
2025-08-20 09:04:16 -0600

    js actual 2

--- src/js/index.js
+++ src/js/index.js
@@ -5,11 +5,19 @@ let currentPlayer = 'X';
 console.log(cells.length)
 
 for (let index = 0; index < cells.length; index++) {
+    
     cells[index].addEventListener("click",function () {
-        if (cells[index].textContent === '') {
-            cells[index].textContent = currentPlayer;
-            currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
+        
+        if (cells[index].textContent == '') {
+            if (currentPlayer === 'X'){
+                cells[index].textContent = currentPlayer
+                currentPlayer = 'O'
+            } else {
+                cells[index].textContent = currentPlayer
+                currentPlayer = 'X'
+            }
         }
-    })
+        
+    });
     
 }
