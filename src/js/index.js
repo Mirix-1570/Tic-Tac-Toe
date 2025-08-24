a515e20235941a320682bb2dda704e7d90faf20b
Mirix-1570 <miricruz1570@gmail.com>
2025-08-24 17:19:12 -0600

    js actual 5

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,17 @@
+
+
+//Llama del html al tablero
 const board = document.getElementById('board');
-const btnReiniciar = document.getElementById('btnReiniciar');
+
+//Llama del html las casillas
 const cells = board.getElementsByClassName('cell');
+console.log(cells);
+
+
+//Llama del html al boton de reiniciar
+const btnReiniciar = document.getElementById('btnReiniciar');
+
+//Define las combinaciones ganadoras
 const combinacionesGanadoras = [
     // Horizontales
     [0, 1, 2],
@@ -14,35 +25,40 @@ const combinacionesGanadoras = [
     [0, 4, 8],
     [2, 4, 6]
 ];
-let currentPlayer = 'X';
+
+//Declara quien es el jugador
+const playerComputer = 'O';
+const playerHuman = 'X';
+let currentPlayer = playerHuman;
+
+
+//Partida en curso
 let juegoActivo = true;
 
+//Declara contadores de victorias por jugador
+let contadorX = parseInt(localStorage.getItem('victoriasX')) || 0;
+let contadorO = parseInt(localStorage.getItem('victoriasO')) || 0;
 
+//Funcion del boton reiniciar
 function reiniciarPagina() {
     location.reload();
 }
-
+//Llamado de la funcion del boton reiniciar
 btnReiniciar.addEventListener('click', reiniciarPagina);
 
+//Condicional de partida en curso
 if (juegoActivo) {
     currentPlayer = (currentPlayer === 'X') ? 'O' : 'X';
 }
 
+//Recorrido de celdas
 for (let index = 0; index < cells.length; index++) {
     
+    //click con condicional para definir que pasa al darle click a la celda
     cells[index].addEventListener("click",function () {
         if (!juegoActivo) return;
 
-        if (cells[index].textContent == '') {
-            if (currentPlayer === 'X'){
-                cells[index].textContent = currentPlayer
-                currentPlayer = 'O'
-            } else {
-                cells[index].textContent = currentPlayer
-                currentPlayer = 'X'
-            }
-        }
-        
+        cells[index].textContent = playerHuman
 
         function revisarGanador() {
             for (let i = 0; i < combinacionesGanadoras.length; i++) {
@@ -58,6 +74,7 @@ for (let index = 0; index < cells.length; index++) {
                     
                     alert(`¡El jugador ${celdaA.textContent} ha ganado!`);
                     juegoActivo = false;
+                    location.reload();
                     return;
                 }
             }
@@ -65,7 +82,26 @@ for (let index = 0; index < cells.length; index++) {
         
     }
     revisarGanador();
+
     
+        
+       function checkCells() {
+        let isempty = false
+        while(!isempty){
+            const randomcell = Math.floor(Math.random() * 9);
+            if (cells[randomcell].textContent === "") {
+            cells[randomcell].textContent = playerComputer
+            isempty = true
+       }
+       }
+        }
+
+    setTimeout(
+        function () {
+            checkCells();
+        }, 1000
+    )
+
     });
     
 }
